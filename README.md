7a13d934294d4e82487622288135349213d42ed6
graydovee <1036308853@qq.com>
2020-06-21 23:56:30 +0800

    完善文档

--- README.md
+++ README.md
@@ -24,7 +24,20 @@ public class MyAuthorityConfiguration implements AuthorityConfigure {
 }
 ```
 
-6. 若要修改认证返回结果，可重写并注册以下接口：
+6. 若要在controller中获取用户信息，可使用`@CurrentUser`注解，示例如下：
+   
+```java
+@RestController
+public class Mycontroller {
+    @GetMapping("/info")
+    public User getInfo(@CurrentUser User user){
+        return user;
+    }
+}
+```
+
+7. 若要修改认证返回结果，可重写并注册以下接口：
+
    1. 登陆成功：实现`AuthenticationSuccessHandler`接口
    2. 登陆失败：继承并重写`AuthenticationDenyHandler`抽象类
    3. 访问拒绝，因未登录：继承并重写`AuthenticationDenyHandler`抽象类
--- jwt-web/src/main/java/cn/graydove/Application.java
+++ jwt-web/src/main/java/cn/graydove/Application.java
@@ -1,5 +1,7 @@
 package cn.graydove;
 
+import cn.graydove.security.annotation.CurrentUser;
+import cn.graydove.security.userdetails.support.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +16,13 @@ public class Application {
     }
 
     @PostMapping("/register")
-    public String register(){
+    public String register(@CurrentUser User user){
         return "success";
     }
+
+    @PostMapping("/info")
+    public User getInfo(@CurrentUser User user){
+        return user;
+    }
+
 }
